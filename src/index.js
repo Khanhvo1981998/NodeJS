205eab0d4c6ef69e2398c4603e30214629cef306
Duy Khanh <duykhanh@Duy-Khanh-MacBook-Pro.local>
2023-04-29 15:49:45 +0700

    create router & controllers

--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@ const handlebars = require('express-handlebars').create({
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
 
 // HTTP log
 app.use(morgan('combined'))
@@ -18,13 +24,8 @@ app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resource/views'))
 
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+route(app)
 
-app.get('/news', (req, res) => {
-    res.render('news')
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
